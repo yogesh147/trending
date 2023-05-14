a20b7ebacdf20b94a79dff1b56a2a9712fbd4401
yogesh147 <yogeshch147@gmail.com>
2023-05-14 21:36:34 +0530

    add site

---
+++ src/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>JavaTpoint site</h1>
+      </div>
+    );
+  }
+}
+export default Header;
