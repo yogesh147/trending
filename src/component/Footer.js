68fe7ce50950532faa375ea6b701e97a2bb95183
yogesh147 <yogeshch147@gmail.com>
2023-05-22 21:33:13 +0530

    header changes done

---
+++ src/component/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {  
+    render() {  
+       return (  
+          <div>  
+             <h2>www.javatpoint.com Footer</h2>  
+            </div> 
+        );  
+    }
+ }
+ 
+ export default Footer;
