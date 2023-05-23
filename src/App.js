b1c2ed676305c56cdb2cbea45b9d26a36a53c9a9
yogesh147 <yogeshch147@gmail.com>
2023-05-23 23:09:18 +0530

    body changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './component/Header.js';
+import Body from './component/Body.js';
 
 export default function App() {
    return (
       <>
          <Header />
-    
+         <Body />
       </>
    );
 }
--- src/component/Body.js
+++ src/component/Body.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
-class Body extends React.Component {  
-    render() {  
-       return (  
-          <div>  
-             <h2>www.javatpoint.com Body</h2>  
-            </div> 
-        );  
-    }
- }
- 
- export default Body;
+export default function Body() {
+
+   const body = {
+      borderBottomColor: "#f5f5f52e",
+      borderBottomStyle: "ridge",
+      height: "144px"
+   };
+
+   const col = {
+      backgroundColor: "white",
+      textAlign: "center",
+      fontWeight: "bolder",
+      fontSize: "-webkit-xxx-large",
+      color: "#000000b5"
+   };
+
+   const col2 = {
+      backgroundColor: "white",
+      textAlign: "center",
+      fontWeight: "bolder",
+      fontSize: "-webkit-xxx-large",
+      color: "#4caf50db",
+      borderBottomColor: "#f5f5f52e",
+      borderBottomStyle: "ridge"
+   };
+
+   return (
+      <Container fluid>
+         <Row style={body}>
+            <Col style={col} sm={12} md={12} lg={12} xl={12} xxl={12} xs={12} >
+               Welcome to the
+            </Col>
+            <Col style={col2} sm={12} md={12} lg={12} xl={12} xxl={12} xs={12} >
+               Mobile Store
+            </Col>
+         </Row>
+      </Container>
+   );
+
+}
