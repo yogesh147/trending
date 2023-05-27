c4b29cb79b769c08fa817cb92c34dc9755cee7b9
yogesh147 <yogeshch147@gmail.com>
2023-05-27 19:27:07 +0530

    add button

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 
 export default function Body() {
 
@@ -27,8 +28,27 @@ export default function Body() {
       borderBottomStyle: "ridge"
    };
 
+   const selectedBtn = {
+      backgroundColor: "#1a73e8",
+      color: "white"
+   };
+
+   const unSelectedBtn = {
+      backgroundColor: "white",
+      color: "#1a73e8"
+   };
+
+   const [isVisible, setVisible] = useState(true);
+
+   const btnToggle = (type) => {
+      if ((type === "mobiles") && isVisible) return;
+      if ((type === "accessories") && !isVisible) return;
+      setVisible((current) => !current);
+   };
+
    return (
       <Container fluid>
+         
          <Row style={body}>
             <Col style={col} sm={12} md={12} lg={12} xl={12} xxl={12} xs={12} >
                Welcome to the
@@ -37,7 +57,28 @@ export default function Body() {
                Mobile Store
             </Col>
          </Row>
+
+         <Row>
+            <Col style={col} sm={12} md={12} lg={12} xl={12} xxl={12} xs={12} >
+
+                  <Button variant="outline-primary"  overlaytrigger="true" 
+                     style={isVisible ? selectedBtn : unSelectedBtn}
+                     onClick={() => btnToggle("mobiles")} >
+                     Mobiles
+                  </Button>
+
+               {' '}
+               <Button variant="outline-primary"
+                  style={!isVisible ? selectedBtn : unSelectedBtn}
+                  onClick={() => btnToggle("accessories")} >
+                  Accessories
+               </Button>
+
+            </Col>
+         </Row>
+
       </Container>
    );
+
 
 }
--- src/component/Header.js
+++ src/component/Header.js
@@ -2,7 +2,8 @@ import React from 'react';
 import '../css/custom.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
-import logo from "../images/brand-logo.jpg";
+import black from "../images/black.png";
+import login from "../images/login-logo.png";
 
 export default function Header() {
 
@@ -16,7 +17,8 @@ export default function Header() {
             <Container fluid>
                   <Row style={header}>
                         <Col sm={12} md={12} lg={12} xl={12} xxl={12} xs={12} >
-                              <img src={logo} width={60} height={60} alt="brand-logo" />
+                              <img src={login} width={70} height={60} alt="login-logo" />
+                              <img src={black} width={50} height={50} alt="black" style={{float: "right"}} />
                         </Col>
                   </Row>
             </Container>
