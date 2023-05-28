ccd2391bee38787d384cf8b121f7695dcec3ff42
yogesh147 <yogeshch147@gmail.com>
2023-05-28 12:17:35 +0530

    latest div col changes

--- src/component/Body.js
+++ src/component/Body.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Tooltip from "./Tooltip";
 import "./Body.css";
+import '../css/custom.css';
 
 export default function Body() {
    const [isVisible, setVisible] = useState(true);
@@ -30,7 +31,7 @@ export default function Body() {
 
          <Row>
             <Col className="Body-Col" sm={12} md={12} lg={12} xl={12} xxl={12} xs={12} >
-              
+
                <Tooltip content={mobileToolTip} direction="left">
                   <Button variant="outline-primary"
                      className={isVisible ? "Body-selectedBtn" : "Body-unSelectedBtn"}
@@ -51,6 +52,29 @@ export default function Body() {
                </Tooltip>
             </Col>
          </Row>
+
+         <Row className="pt-20">
+            <Col sm={12} md={2} lg={2} xl={2} xxl={2} xs={12}>
+            </Col>
+
+            <Col sm={12} md={8} lg={8} xl={8} xxl={8} xs={12}  >
+               <Row className="height-300">
+
+                  <Col sm={12} md={4} lg={4} xl={4} xxl={4} xs={12} style={{ borderStyle: "solid" }}>
+                  </Col>
+                  <Col sm={12} md={4} lg={4} xl={4} xxl={4} xs={12}>
+                  </Col>
+                  <Col sm={12} md={4} lg={4} xl={4} xxl={4} xs={12}>
+                  </Col>
+
+               </Row>
+
+            </Col>
+            <Col sm={12} md={2} lg={2} xl={2} xxl={2} xs={12}>
+            </Col>
+
+         </Row>
+
       </Container>
    );
 
--- src/component/Header.js
+++ src/component/Header.js
@@ -3,8 +3,7 @@ import '../css/custom.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import black from "../images/black.png";
-import login from "../images/login-logo.png";
-import "../css/custom.css";
+import logo from "../images/brand-logo.jpg";
 
 export default function Header() {
 
@@ -12,7 +11,7 @@ export default function Header() {
             <Container fluid>
                   <Row className="header">
                         <Col sm={12} md={12} lg={12} xl={12} xxl={12} xs={12} >
-                              <img src={login} width={70} height={60} alt="login-logo" />
+                              <img src={logo} width={70} height={60} alt="brand-logo" />
                               <img src={black} width={50} height={50} alt="black" style={{ float: "right" }} />
                         </Col>
                   </Row>
