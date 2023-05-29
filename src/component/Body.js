8767a36a8d75f18188cf7e204dac174dd480bf12
yogesh147 <yogeshch147@gmail.com>
2023-05-29 14:39:14 +0530

    responsive changes

--- src/component/Body.js
+++ src/component/Body.js
@@ -23,19 +23,17 @@ export default function Body() {
 
    return (
       <Container fluid>
-
-         <Row className="Body-Block">
-            <Col className="Body-Col" sm={12} md={12} lg={12} xl={12} xxl={12} xs={12} >
-               Welcome to the
+         <Row>
+            <Col sm={12} md={12} lg={12} xl={12} xxl={12} xs={12} >
+               <div className="Body-Col black-color">Welcome to the</div>
             </Col>
-            <Col className="Body-Col2" sm={12} md={12} lg={12} xl={12} xxl={12} xs={12} >
-               Mobile Store
+            <Col sm={12} md={12} lg={12} xl={12} xxl={12} xs={12} >
+               <div className="Body-Col green-color">Mobile Store</div>
             </Col>
          </Row>
-
-         <Row>
-            <Col className="Body-Col" sm={12} md={12} lg={12} xl={12} xxl={12} xs={12} >
-
+         <Row className="pt-3">
+            <Col sm={12} md={12} lg={12} xl={12} xxl={12} xs={12} >
+               <div className="text-center">
                <Tooltip content={mobileToolTip} direction="left">
                   <Button variant="outline-primary"
                      className={isVisible ? "Body-selectedBtn" : "Body-unSelectedBtn"}
@@ -54,55 +52,51 @@ export default function Body() {
                   </Button>
 
                </Tooltip>
+               </div>  
             </Col>
-         </Row>
+         </Row> 
 
-         <Row className="pt-20">
-            <Col sm={12} md={2} lg={2} xl={2} xxl={2} xs={12}>
-            </Col>
+         {isVisible ? (
+            <Row className="pt-20">
+               <Col sm={12} md={2} lg={2} xl={2} xxl={2} xs={12}>
+               </Col>
 
-            <Col sm={12} md={8} lg={8} xl={8} xxl={8} xs={12}  >
-               <Row className="ht-300 mb-50 ">
+               <Col sm={12} md={8} lg={8} xl={8} xxl={8} xs={12}  >
+                  <Row className="ht-300 mb-50 ">
 
-                  <Col sm={12} md={4} lg={4} xl={4} xxl={4} xs={12}>
-                     <div className="bg-light-black pd-1">
-                        <img src={mobile1} className='zoom' alt="Pixel 7a" />
-                        <h6 className="text-center">Pixel 7a</h6>
-                     </div>
-                  </Col>
+                     <Col sm={12} md={4} lg={4} xl={4} xxl={4} xs={12}>
+                        <div className="bg-light-black pd-1">
+                           <img src={mobile1} className='zoom' alt="Pixel 7a" />
+                           <h6 className="text-center">Pixel 7a</h6>
+                        </div>
+                     </Col>
 
-                  <Col sm={12} md={4} lg={4} xl={4} xxl={4} xs={12}>
-                     <div className="bg-light-black pd-1">
-                        <img src={mobile2} className='zoom' alt="Pixel 7 Pro" />
-                        <h6 className="text-center">Pixel 7 Pro</h6>
-                     </div>
-                  </Col>
+                     <Col sm={12} md={4} lg={4} xl={4} xxl={4} xs={12}>
+                        <div className="bg-light-black pd-1">
+                           <img src={mobile2} className='zoom' alt="Pixel 7 Pro" />
+                           <h6 className="text-center">Pixel 7 Pro</h6>
+                        </div>
+                     </Col>
 
-                  <Col sm={12} md={4} lg={4} xl={4} xxl={4} xs={12}>
-                     <div className="bg-light-black pd-1">
-                        <img src={mobile3} className='zoom' alt="Pixel 7" />
-                        <h6 className="text-center">Pixel 7</h6>
-                     </div>
-                  </Col>
+                     <Col sm={12} md={4} lg={4} xl={4} xxl={4} xs={12}>
+                        <div className="bg-light-black pd-1">
+                           <img src={mobile3} className='zoom' alt="Pixel 7" />
+                           <h6 className="text-center">Pixel 7</h6>
+                        </div>
+                     </Col>
+                  </Row>
+               </Col>
+               <Col sm={12} md={2} lg={2} xl={2} xxl={2} xs={12}>
+               </Col>
 
-               </Row>
+            </Row>) : (<Row className="ht-300">
 
-               <Row className="ht-300">
-
-                  <Col sm={12} md={4} lg={4} xl={4} xxl={4} xs={12}>
-                     <div className="bg-light-black pd-1">
-                        <img src={mobile4} className='zoom' alt="Pixel 6a" />
-                        <h6 className="text-center">Pixel 6a</h6>
-                     </div>
-                  </Col>
-
-               </Row>
-
-            </Col>
-            <Col sm={12} md={2} lg={2} xl={2} xxl={2} xs={12}>
-            </Col>
-
-         </Row>
+               <Col sm={12} md={4} lg={4} xl={4} xxl={4} xs={12}>
+                  <div className="bg-light-black pd-1">
+                     <img src={mobile4} className='zoom' alt="Pixel 6a" />
+                     <h6 className="text-center">Pixel 6a</h6>
+                  </div>
+               </Col> </Row>)}
 
       </Container>
    );
