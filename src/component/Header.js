fbc026f9a51f8ee8c2af16bb0e78e451d91b59f3
yogesh147 <yogeshch147@gmail.com>
2023-06-12 23:17:44 +0530

    add webp images

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,8 +2,9 @@ import React from 'react';
 import '../css/custom.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
-import black from "../images/black.png";
-import logo from "../images/brand-logo.jpg";
+import black from "../images/black.webp";
+import logo from "../images/favicon.ico";
+
 
 export default function Header() {
       return (
@@ -11,7 +12,7 @@ export default function Header() {
                   <Row className="header">
                         <Col sm={12} md={12} lg={12} xl={12} xxl={12} xs={12} className="sticky" >
                               <a href="/home" target="_self" rel="noreferrer">
-                                    <img src={logo} width={70} height={60} alt="brand-logo" />
+                                    <img src={logo} width={40} height={40} alt="brand-logo" />
                                     <img src={black} width={50} height={50} alt="black" style={{ float: "right" }} />
                               </a>
                         </Col>
