ad4d3d4f25fd3dd49b56da21af6b5917044ab719
yogesh147 <yogeshch147@gmail.com>
2023-07-18 19:04:48 +0530

    add image links

--- src/component/Body.js
+++ src/component/Body.js
@@ -4,32 +4,54 @@ import Button from 'react-bootstrap/Button';
 import Tooltip from "./Tooltip";
 import "./Body.css";
 import '../css/custom.css';
-import Pixel7a from "../images/Pixel7a.webp";
-import Pixel7Pro from "../images/Pixel7Pro.webp";
-import Pixel7 from "../images/pixel7.webp";
-import Pixel6a from "../images/Pixel6a.webp";
-import Pixel7aCase from "../images/Pixel7aCase.webp";
-import Iphone14 from "../images/iphone14ProMax.webp";
-import VivoT2x from "../images/VivoT2x.webp";
-import POCOX55G from "../images/POCOX55G.webp";
-import realme10Pro5g from "../images/realme10Pro5g.webp";
-import oneplus from "../images/oneplus.webp";
-import boat550 from "../images/boat550.webp";
+
 import buy from "../images/buy.webp";
 import axios from "axios";
 
 export default function Body() {
-   const url = "http://localhost:8080/image/iphone14ProMax.webp";
+   const url = "http://localhost:8080/image/";
    const [isVisible, setVisible] = useState(true);
    const [data, setData] = useState([]);
 
-   useEffect(() => {
+   const [mobiles] = useState([
+      { id: 1, imageName: "Pixel7a.webp", name: "Pixel 7a", width: "80%", href: "https://amzn.to/3oFHoea", src: "" },
+      { id: 2, imageName: "Pixel7Pro.webp", name: "Pixel 7 Pro", width: "80%", href: "https://amzn.to/3ON7S7Y", src: "" },
+      { id: 3, imageName: "Pixel7.webp", name: "Pixel 7", width: "80%", href: "https://amzn.to/42ihYkx", src: "" },
+      { id: 4, imageName: "Pixel6a.webp", name: "Pixel 6a", width: "80%", href: "https://amzn.to/42kC5OZ", src: "" },
+      { id: 5, imageName: "Iphone14ProMax.webp", name: "iPhone 14 Pro Max", width: "80%", href: "https://amzn.to/3WJet5k", src: "" },
+      { id: 6, imageName: "VivoT2x.webp", name: "Vivo T2x 5g", width: "65%", href: "https://amzn.to/45DAQgH", src: "" },
+      { id: 7, imageName: "POCOX55G.webp", name: "POCO X5 5G", width: "60%", href: "https://amzn.to/3ONOkAs", src: "" },
+      { id: 8, imageName: "Realme10Pro5g.webp", name: "Realme 10 Pro 5G", width: "70%", href: "https://amzn.to/3N6YJ9a", src: "" },
+      { id: 8, imageName: "Oneplus.webp", name: "OnePlus Nord 5G", width: "70%", href: "https://amzn.to/3OPErSY", src: "" }
+   ]);
 
-      axios.get(url, { responseType: "arraybuffer" })
-         .then((response) => {
-            const base64String = window.btoa(String.fromCharCode(...new Uint8Array(response.data)));
-            setData(base64String);
-         });
+   const [accessories] = useState([
+      { id: 101, imageName: "Pixel7aCase.webp", name: "Pixel 7a Case", width: "80%", href: "https://amzn.to/3OPrd8x", src: "" },
+      { id: 102, imageName: "Boat550.webp", name: "BoAt Rockerz 550", width: "80%", href: "https://amzn.to/3MCBySI", src: "" }
+   ]);
+
+   useEffect(() => {
+      for (let i = 0; i < mobiles.length; i++) {
+         axios.get(url + mobiles[i].imageName, { responseType: "arraybuffer" })
+            .then((response) => {
+               const base64String = window.btoa(String.fromCharCode(...new Uint8Array(response.data)));
+               if (base64String != null) {
+                  setData('data:image/png;base64,' + base64String);
+                  mobiles[i].src = 'data:image/png;base64,' + base64String;
+               }
+            });
+      }
+
+      for (let i = 0; i < accessories.length; i++) {
+         axios.get(url + accessories[i].imageName, { responseType: "arraybuffer" })
+            .then((response) => {
+               const base64String = window.btoa(String.fromCharCode(...new Uint8Array(response.data)));
+               if (base64String != null) {
+                  setData('data:image/png;base64,' + base64String);
+                  accessories[i].src = 'data:image/png;base64,' + base64String;
+               }
+            });
+      }
 
    }, []);  
 
@@ -43,23 +65,6 @@ export default function Body() {
       if ((type === "accessories") && !isVisible) return;
       setVisible((current) => !current);
    };
-
-   const [mobiles] = useState([
-      { id: 1, imgName: Pixel7a, name: "Pixel 7a", width: "80%", href: "https://amzn.to/3oFHoea" },
-      { id: 2, imgName: Pixel7Pro, name: "Pixel 7 Pro", width: "80%", href: "https://amzn.to/3ON7S7Y" },
-      { id: 3, imgName: Pixel7, name: "Pixel 7", width: "80%", href: "https://amzn.to/42ihYkx" },
-      { id: 4, imgName: Pixel6a, name: "Pixel 6a", width: "80%", href: "https://amzn.to/42kC5OZ" },
-      { id: 5, imgName: Iphone14, name: "iPhone 14 Pro Max", width: "80%", href: "https://amzn.to/3WJet5k" },
-      { id: 6, imgName: VivoT2x, name: "Vivo T2x 5g", width: "65%", href: "https://amzn.to/45DAQgH" },
-      { id: 7, imgName: POCOX55G, name: "POCO X5 5G", width: "60%", href: "https://amzn.to/3ONOkAs" },
-      { id: 8, imgName: realme10Pro5g, name: "Realme 10 Pro 5G", width: "70%", href: "https://amzn.to/3N6YJ9a" },
-      { id: 8, imgName: oneplus, name: "OnePlus Nord 5G", width: "70%", href: "https://amzn.to/3OPErSY" }
-   ]);
-
-   const [accessories] = useState([
-      { id: 101, imgName: Pixel7aCase, name: "Pixel 7a Case", width: "80%", href: "https://amzn.to/3OPrd8x" },
-      { id: 102, imgName: boat550, name: "BoAt Rockerz 550", width: "80%", href: "https://amzn.to/3MCBySI" }
-   ]);
 
    return (
       <Container fluid>
@@ -102,15 +107,13 @@ export default function Body() {
                </Col>
 
                <Col sm={12} md={6} lg={6} xl={8} xxl={8} xs={12}  >
-               <img src={`data:image/png;base64,${data}`} alt="buy" />
                   <Row>
                      {
-                      
                         mobiles.map((item, index) =>
                            <Col key={index} className="pd-4 text-center" sm={12} md={4} lg={4} xl={4} xxl={4} xs={12}>
                               <div className="bg-light-black pd-1 mt-28 mb-4">
                                  <a href={item.href} target="_blank" rel="noreferrer">
-                                    <img src={item.imgName} className='zoom' width={item.width} alt={item.name} />
+                                    <img src={item.src} className='zoom' width={item.width} alt={item.name} />
                                  </a>
                                  <h6>{item.name} </h6>
                                  <Tooltip content={cart} direction="bottom" className="display-none">
@@ -144,7 +147,7 @@ export default function Body() {
                               <Col key={index} className="text-center" sm={12} md={4} lg={4} xl={4} xxl={4} xs={12}>
                                  <div className="bg-light-black mt-28 mb-4 pd-1">
                                     <a href={item.href} target="_blank" rel="noreferrer">
-                                       <img src={item.imgName} className='zoom' width={item.width} alt={item.name} />
+                                       <img src={item.src} className='zoom' width={item.width} alt={item.name} />
                                     </a>
                                     <h6>{item.name} </h6>
                                     <Tooltip content={cart} direction="bottom" className="display-none">
